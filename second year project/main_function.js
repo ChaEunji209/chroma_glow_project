8bba6beadc2142867909938953190745c299f10e
Thandar Su <lovecherry955@gamil.com>
2025-10-16 18:45:12 +0630

    final commit

--- second year project/main_function.js
+++ second year project/main_function.js
@@ -17,42 +17,37 @@
     fair: {
       title: "Fair Skin",
       text: `
-      <p><span class="hover-link">
-    <div class="hover-card">
-        <a href="https://skincaregeeks.com/fair-skin-tone/" target="_blank">Fair skin tone refers to a light complexion characterized by lower melanin levels, making it more sensitive and prone to conditions like sunburn.</a>
-      </div>
-    </span>
-      </p>
+        <p>Fair skin tone refers to a light complexion characterized by lower melanin levels, making it more sensitive and prone to conditions like sunburn.</p>
       
       <p><strong>Characteristics of Fair Skin:</strong></p>
   
       <p><strong>Melanin Levels:</strong>Fair skin has less melanin, making it lighter. This leads to increased sensitivity to UV radiation and a higher risk of sunburn and skin damage.</p>
-      <p><strong>Skin Sensitivity:</strong>People with fair skin often experience redness and irritation more easily. They must choose skincare products carefully to avoid reactions.</p>`
+      <p><strong>Skin Sensitivity:</strong>People with fair skin often experience redness and irritation more easily. They must choose skincare products carefully to avoid reactions.</p>
+      <span class="hover-link">
+    <div class="hover-card">
+        <a href="https://skincaregeeks.com/fair-skin-tone/" target="_blank">Learn More &rarr;</a>
+      </div>
+    </span>`
     
     },
     light: {
       title: "Light Skin",
-      text: `<p>
-        <span class="hover-link">
-    <div class="hover-card">
-    <a href="https://en.wikipedia.org/wiki/Light_skin" target="_blank">Light skin tone refers to a complexion that is characterized by low levels of melanin, resulting in a pale appearance that can range from ivory to light beige.</a>
-    </div>
-  </span> 
+      text: `<p>Light skin tone refers to a complexion that is characterized by low levels of melanin, resulting in a pale appearance that can range from ivory to light beige.
   </p>
 
       <p><strong>Characteristics of Light Skin Tone</strong></p>
 
       <p><strong>Melanin Levels:</strong> Light skin is primarily determined by the amount of melanin present in the skin. Melanin is the pigment responsible for skin color, and lower levels result in lighter skin tones.</p> 
-      <p><strong>Fitzpatrick Scale:</strong> Light skin is often categorized as Type I or Type II on the Fitzpatrick scale, which classifies skin types based on their reaction to sun exposure. Type I skin always burns and never tans, while Type II burns easily and tans minimally.</p>`
+      <p><strong>Fitzpatrick Scale:</strong> Light skin is often categorized as Type I or Type II on the Fitzpatrick scale, which classifies skin types based on their reaction to sun exposure. Type I skin always burns and never tans, while Type II burns easily and tans minimally.</p>
+      <span class="hover-link">
+        <div class="hover-card">
+        <a href="https://en.wikipedia.org/wiki/Light_skin" target="_blank">Learn More &rarr;</a>
+        </div>
+      </span>`
     },
     medium: {
       title: "Medium Skin",
-      text: `<p>
-        <span class="hover-link">
-          <div class="hover-card">
-            <a href="https://www.beautyanswered.com/what-is-medium-skin.htm" target="_blank">Medium skin tone refers to a skin color that is neither too light nor too dark, often characterized by a range of shades from light tan to olive.</a>
-          </div>
-        </span></p>
+      text: `<p>Medium skin tone refers to a skin color that is neither too light nor too dark, often characterized by a range of shades from light tan to olive.</p>
 
       <p><strong>Characteristics of Medium Skin Tone</strong></p>
 
@@ -61,48 +56,53 @@
        <p><strong> Warm:</strong> Yellow or golden hues.</p>
         <p><strong>Cool:</strong> Pink or rosy hues.</p>
        <p><strong> Neutral:</strong> A balance between warm and cool tones.</p>
-        <p><strong>Olive:</strong> A mix of green and gray hues.</p>`
+        <p><strong>Olive:</strong> A mix of green and gray hues.</p>
+        <span class="hover-link">
+          <div class="hover-card">
+            <a href="https://www.beautyanswered.com/what-is-medium-skin.htm" target="_blank">Learn More &rarr;</a>
+          </div>
+        </span>`
     },
     tan: {
       title: "Tan Skin",
-      text: `<p>
-        <span class="hover-link">
-        <div class="hover-card">
-          <a href="https://duskyskin.com/what-is-tan-skin-tone/" target="_blank">Tan skin tone refers to a range of light to dark brown skin shades that result from sun exposure or natural pigmentation, characterized by warm undertones.</a>
-        </div>
-        </span></p>
+      text: `<p>Tan skin tone refers to a range of light to dark brown skin shades that result from sun exposure or natural pigmentation, characterized by warm undertones.</p>
 
       <p><strong>Characteristics of Tan Skin Tone</strong></p>
 
       <p><strong>Color Range:</strong> tan skin can vary from light brown to deep mocha,often described as having a sun-kissed appearance. It typically includes shades that are warm golden brown to darker hues.
-      <p><strong>Undertones:</strong> Tan skin tones may have yellow, brown, or olive undertones, which can affect how colors appear on the skin and influence makeup choices.</p>`
+      <p><strong>Undertones:</strong> Tan skin tones may have yellow, brown, or olive undertones, which can affect how colors appear on the skin and influence makeup choices.</p>
+      <span class="hover-link">
+        <div class="hover-card">
+          <a href="https://duskyskin.com/what-is-tan-skin-tone/" target="_blank">Learn More &rarr;</a>
+        </div>
+        </span>`
     },
     deep: {
       title: "Deep Skin",
-      text: `<p>
-        <span class="hover-link">
-          <div class="hover-card">
-            <a href="https://claudianour.com/blogs/modest-makeup/deep-skin-tones" target="_blank">Deep skin tone is characterized by a rich, dark color that can range from dark brown to ebony, often with a natural radiance and minimal sunburn risk.</a>
-          </div>
-        </span></p>
+      text: `<p>Deep skin tone is characterized by a rich, dark color that can range from dark brown to ebony, often with a natural radiance and minimal sunburn risk.</p>
 
       <p><strong>Characteristics of Deep Skin Tone</strong></p>
 
-      <p><strong>Color Range:</strong> Deep skin toens encompass a variety of shades, from dark brown to ebony.This skin tone typically has a high level of melanin content provides more protection against UV rays.</p>`
+      <p><strong>Color Range:</strong> Deep skin toens encompass a variety of shades, from dark brown to ebony.This skin tone typically has a high level of melanin content provides more protection against UV rays.</p>
+      <span class="hover-link">
+          <div class="hover-card">
+            <a href="https://claudianour.com/blogs/modest-makeup/deep-skin-tones" target="_blank">Learn More &rarr;</a>
+          </div>
+        </span>`
 
     },
     dark: {
       title: "Dark Skin",
-      text: `<p>
-        <span class="hover-link">
-          <div class="hover-card">
-            <a href="https://en.wikipedia.org/wiki/Dark_skin" target="_blank">Dark skin tone refers to skin that is rich in melanin, providing natural protection against UV radiation and contributing to a range of cultural and aesthetic identities.</a>
-          </div>
-        </span></p>
+      text: `<p>Dark skin tone refers to skin that is rich in melanin, providing natural protection against UV radiation and contributing to a range of cultural and aesthetic identities.</p>
 
       <p><strong>Characteristics of Dark Skin Tone</strong></p>
 
-      <p><strong>Malanin Content:</strong> Dark skin is characterized by a high concentration of malanin, partiaculary eumelanin,which gives the skin its daker hue.This pigmentation provides better protection against the harmful effects of ultraviolet(UV) radiation,helping to preserve folate levels and reduce the risk of DNA damage.</p>`
+      <p><strong>Malanin Content:</strong> Dark skin is characterized by a high concentration of malanin, partiaculary eumelanin,which gives the skin its daker hue.This pigmentation provides better protection against the harmful effects of ultraviolet(UV) radiation,helping to preserve folate levels and reduce the risk of DNA damage.</p>
+      <span class="hover-link">
+          <div class="hover-card">
+            <a href="https://en.wikipedia.org/wiki/Dark_skin" target="_blank">Learn More &rarr;</a>
+          </div>
+        </span>`
     }
   };
 
